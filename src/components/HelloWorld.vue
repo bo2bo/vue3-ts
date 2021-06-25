bf172fc24df87c763d86587d741d89c8563e98bc
hongbo_zheng@qingsongchou.com <hongbo_zheng@qingsongchou.com>
2021-06-25 16:11:26 +0800

    element

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <el-button type="primary">主要按钮</el-button>
   </div>
 </template>
 
@@ -16,8 +17,6 @@ export default class HelloWorld extends Vue {
   msg!: string;
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
---
+++ vue.config.js
@@ -0,0 +1,26 @@
+// vue.config.js
+module.exports = {
+    outputDir: 'dist', // 打包的目录
+    lintOnSave: true, // 在保存时校验格式
+    productionSourceMap: false, // 生产环境是否生成 SourceMap
+    devServer: {
+        open: true, // 启动服务后是否打开浏览器
+        overlay: { // 错误信息展示到页面
+            warnings: true,
+            errors: true
+        },
+        host: '0.0.0.0',
+        port: 8066, // 服务端口
+        https: false,
+        hotOnly: false,
+        // proxy: { // 设置代理
+        //   '/api': {
+        //     target: host,
+        //     changeOrigin: true,
+        //     pathRewrite: {
+        //       '/api': '/',
+        //     }
+        //   },
+        // },
+    },
+}
